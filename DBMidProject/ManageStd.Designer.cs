132ba56a6dd2f750fb46b864f18963baddb9fae2
fasitahir <fasitahir2019@gmail.com>
2024-02-29 03:41:40 +0500

    Add Student functionality added

---
+++ DBMidProject/ManageStd.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace DBMidProject
+{
+    partial class ManageStd
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.stdFirstName = new System.Windows.Forms.TextBox();
+            this.stdLastName = new System.Windows.Forms.TextBox();
+            this.stdEmail = new System.Windows.Forms.TextBox();
+            this.stdContact = new System.Windows.Forms.TextBox();
+            this.stdRegNo = new System.Windows.Forms.TextBox();
+            this.addStdBtn = new System.Windows.Forms.Button();
+            this.stdDob = new System.Windows.Forms.DateTimePicker();
+            this.stdGender = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(200, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(201, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Last Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(219, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Contact";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(232, 214);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Email";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(220, 281);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 17);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Gender";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(190, 246);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Date of Birth";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(141, 311);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(130, 17);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Registration Number";
+            // 
+            // stdFirstName
+            // 
+            this.stdFirstName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.stdFirstName.Font = new System.Drawing.Font("Yu Gothic UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stdFirstName.Location = new System.Drawing.Point(293, 105);
+            this.stdFirstName.Name = "stdFirstName";
+            this.stdFirstName.Size = new System.Drawing.Size(100, 16);
+            this.stdFirstName.TabIndex = 1;
+            // 
+            // stdLastName
+            // 
+            this.stdLastName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.stdLastName.Font = new System.Drawing.Font("Yu Gothic UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stdLastName.Location = new System.Drawing.Point(293, 140);
+            this.stdLastName.Name = "stdLastName";
+            this.stdLastName.Size = new System.Drawing.Size(100, 16);
+            this.stdLastName.TabIndex = 2;
+            // 
+            // stdEmail
+            // 
+            this.stdEmail.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.stdEmail.Font = new System.Drawing.Font("Yu Gothic UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stdEmail.Location = new System.Drawing.Point(293, 216);
+            this.stdEmail.Name = "stdEmail";
+            this.stdEmail.Size = new System.Drawing.Size(100, 16);
+            this.stdEmail.TabIndex = 4;
+            // 
+            // stdContact
+            // 
+            this.stdContact.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.stdContact.Font = new System.Drawing.Font("Yu Gothic UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stdContact.Location = new System.Drawing.Point(293, 181);
+            this.stdContact.Name = "stdContact";
+            this.stdContact.Size = new System.Drawing.Size(100, 16);
+            this.stdContact.TabIndex = 3;
+            // 
+            // stdRegNo
+            // 
+            this.stdRegNo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.stdRegNo.Font = new System.Drawing.Font("Yu Gothic UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stdRegNo.Location = new System.Drawing.Point(293, 313);
+            this.stdRegNo.Name = "stdRegNo";
+            this.stdRegNo.Size = new System.Drawing.Size(100, 16);
+            this.stdRegNo.TabIndex = 7;
+            // 
+            // addStdBtn
+            // 
+            this.addStdBtn.Location = new System.Drawing.Point(293, 360);
+            this.addStdBtn.Name = "addStdBtn";
+            this.addStdBtn.Size = new System.Drawing.Size(75, 23);
+            this.addStdBtn.TabIndex = 9;
+            this.addStdBtn.Text = "Add Student";
+            this.addStdBtn.UseVisualStyleBackColor = true;
+            this.addStdBtn.Click += new System.EventHandler(this.addStdBtn_Click);
+            // 
+            // stdDob
+            // 
+            this.stdDob.Location = new System.Drawing.Point(293, 246);
+            this.stdDob.Name = "stdDob";
+            this.stdDob.Size = new System.Drawing.Size(200, 20);
+            this.stdDob.TabIndex = 5;
+            // 
+            // stdGender
+            // 
+            this.stdGender.FormattingEnabled = true;
+            this.stdGender.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.stdGender.Location = new System.Drawing.Point(293, 281);
+            this.stdGender.Name = "stdGender";
+            this.stdGender.Size = new System.Drawing.Size(121, 21);
+            this.stdGender.TabIndex = 6;
+            // 
+            // ManageStd
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.Controls.Add(this.stdGender);
+            this.Controls.Add(this.stdDob);
+            this.Controls.Add(this.addStdBtn);
+            this.Controls.Add(this.stdRegNo);
+            this.Controls.Add(this.stdContact);
+            this.Controls.Add(this.stdEmail);
+            this.Controls.Add(this.stdLastName);
+            this.Controls.Add(this.stdFirstName);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "ManageStd";
+            this.Size = new System.Drawing.Size(710, 451);
+            this.Load += new System.EventHandler(this.ManageStd_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox stdFirstName;
+        private System.Windows.Forms.TextBox stdLastName;
+        private System.Windows.Forms.TextBox stdEmail;
+        private System.Windows.Forms.TextBox stdContact;
+        private System.Windows.Forms.TextBox stdRegNo;
+        private System.Windows.Forms.Button addStdBtn;
+        private System.Windows.Forms.DateTimePicker stdDob;
+        private System.Windows.Forms.ComboBox stdGender;
+    }
+}
---
+++ DBMidProject/ManageStd.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace DBMidProject
+{
+    public partial class ManageStd : UserControl
+    {
+        public ManageStd()
+        {
+            InitializeComponent();
+            stdFirstName.KeyDown += TextBox_KeyDown;
+            stdLastName.KeyDown += TextBox_KeyDown;
+            stdContact.KeyDown += TextBox_KeyDown;
+            stdEmail.KeyDown += TextBox_KeyDown;
+            stdDob.KeyDown += TextBox_KeyDown;
+            stdGender.KeyDown += TextBox_KeyDown;
+            stdRegNo.KeyDown += TextBox_KeyDown;
+
+
+
+        }
+
+        private void ManageStd_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+
+                // Check if the current textbox is the last one
+                if (sender == stdRegNo)
+                {
+                    addStdBtn.PerformClick();
+                }
+                
+               else {
+                    // Move to the next control in the tab index order
+                    SelectNextControl((Control)sender, true, true, true, true);
+                }
+            }
+        }
+
+        private void addStdBtn_Click(object sender, EventArgs e)
+        {
+            int gender;
+            var con = Configuration.getInstance().getConnection();
+            SqlCommand cmd = new SqlCommand(
+                "INSERT INTO Person VALUES (@FirstName, @LastName, @Contact, @Email, @DateOfBirth, @Gender);" +
+                "INSERT INTO Student (Id, RegistrationNo) VALUES ((SELECT MAX(Id) FROM Person), @RegistrationNo)",
+                con);
+
+            string selectedGender = stdGender.Text.ToString().ToLower();
+
+            if (selectedGender == "male")
+            {
+                gender = 1;
+            }
+            else if (selectedGender == "female")
+            {
+                gender = 2;
+            }
+            else
+            {
+                gender = 0;
+            }
+
+            string fname = stdFirstName.Text;
+            string lname = stdLastName.Text;
+            string contact = stdContact.Text;
+            string email = stdEmail.Text;
+            string regNo = stdRegNo.Text;
+
+
+            
+            cmd.Parameters.AddWithValue("@FirstName", stdFirstName.Text);
+            cmd.Parameters.AddWithValue("@LastName", stdLastName.Text);
+            cmd.Parameters.AddWithValue("@Contact", stdContact.Text);
+            cmd.Parameters.AddWithValue("@Email", stdEmail.Text);
+            cmd.Parameters.AddWithValue("@Gender", gender);
+            cmd.Parameters.AddWithValue("@DateOfBirth", stdDob.Value);
+            cmd.Parameters.AddWithValue("@RegistrationNo", stdRegNo.Text);
+            
+            cmd.ExecuteNonQuery();
+            
+            MessageBox.Show("Successfully saved");
+
+        }
+    }
+}
